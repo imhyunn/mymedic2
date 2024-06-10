8e8d91eef8375885c4ce284bef62bc103df61557
imhyunn <byuhis938@gmail.com>
2024-06-10 20:55:21 +0900

    add

--- lib/screens/home/wordNote.dart
+++ lib/screens/home/wordNote.dart
@@ -1,8 +1,10 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+
+import '../../config/palette.dart';
 
 class WordNote extends StatefulWidget {
   const WordNote({super.key});
-
   @override
   State<WordNote> createState() => _WordNoteState();
 }
@@ -21,63 +23,156 @@ class _WordNoteState extends State<WordNote> {
     return Scaffold(
       appBar: AppBar(
         title: Text(''),
+        actions: [
+          IconButton(
+              onPressed: () {
+                _addword(context);
+              },
+              icon: Icon(Icons.add))
+        ],
       ),
       body: ListView.separated(
-        padding: EdgeInsets.all(8),
-        itemCount: wordlist.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ListTile(
-                  contentPadding: const EdgeInsets.only(left: 20, right: 30),
-                  horizontalTitleGap: 10,
-                  isThreeLine: true,
-                  leading: Icon(
-                    Icons.volume_up_rounded,
-                    size: 20,
-                  ),
-                  title: Text(
-                    '${wordlist[index]}',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  subtitle: Text(
-                    '${krword[index]}',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  trailing: SizedBox(
-                    height: 100,
-                    child: OutlinedButton(
-                      onPressed: () {},
-                      child: Icon(Icons.add),
-                      style: OutlinedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)))),
+          itemCount: wordlist.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: ListTile(
+                onTap: null,
+                title: Row(
+                  children: [
+                    Container(
+                      child: Icon(
+                        Icons.volume_up_rounded,
+                        size: size.width * 0.055,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          height: 35,
+                          child: Text(
+                            wordlist[index],
+                            style: TextStyle(fontSize: 19),
+                          ),
+                        ),
+                        Container(
+                          height: 35,
+                          child: Text(
+                            krword[index],
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: size.width * 0.48,
+                    ),
+                    Container(
+                      width: size.width * 0.2,
+                      height: size.height * 0.09,
+                      child: DottedBorder(
+                        radius: Radius.circular(20),
+                        color: Colors.grey,
+                        child: Center(child: Icon(Icons.add)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider();
+          }),
+    );
+  }
+
+  Future<void> _addword(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Palette.backColor,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.0)),
+          title: Text(
+            '영단어 추가',
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                maxLength: 20,
+                decoration: InputDecoration(
+                  hintText: '영단어',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black54,
                     ),
                   ),
-                  // trailing: Container(
-                  //     width: 100,
-                  //     height: 100,
-                  //     decoration: BoxDecoration(
-                  //       border: Border.all(width: 2, color: Colors.grey),
-                  //       borderRadius: BorderRadius.circular(10),
-                  //     ),
-                  //     child: IconButton(
-                  //       icon: Icon(Icons.add),
-                  //       onPressed: () {},
-                  //     ))
-                  // titleAlignment: ListTileTitleAlignment.top,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black54,
+                    ),
+                  ),
                 ),
-              ],
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return Divider();
-        },
-      ),
+              ),
+              TextField(
+                maxLength: 20,
+                decoration: InputDecoration(
+                  hintText: '뜻',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black54,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black54,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Palette.buttonColor2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  child: const Text(
+                    '취소',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Palette.buttonColor2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  child: const Text(
+                    '확인',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+          ],
+        );
+      },
     );
   }
 }
