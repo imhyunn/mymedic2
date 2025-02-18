7604ccfb95de316da955bd0d9b3893b26e93e57a
imhyunn <byuhis938@gmail.com>
2025-02-18 20:23:55 +0900

    wordtest animation

--- lib/screens/home/test/wordTest.dart
+++ lib/screens/home/test/wordTest.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mymedic1/screens/myapp.dart';
 
 import '../../../data/folder.dart';
 import '../../../data/word.dart';
@@ -13,15 +14,15 @@ class WordTest extends StatefulWidget {
   State<WordTest> createState() => _WordTestState();
 }
 
-
 class _WordTestState extends State<WordTest> {
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  PageController _pageController = PageController();
+  int _currentIndex = 0;
 
   Future<List<Word>> getWord() async {
     var snapshot = await _firestore
         .collection('words')
         .where('folderId', isEqualTo: widget.folder.id)
-        .orderBy('time', descending: true)
         .get();
     List<Word> words = snapshot.docs.map((element) {
       Map<String, dynamic> map = element.data();
@@ -31,28 +32,125 @@ class _WordTestState extends State<WordTest> {
     return words;
   }
 
+  void _nextQuestion(List<Word> words) {
+    if (_currentIndex < words.length) {
+      setState(() {
+        _currentIndex++;
+      });
+      _pageController.animateToPage(_currentIndex,
+          duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+    }
+  }
+
+  Widget _StartUI() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Card(
+            child: Container(
+                width: 200,
+                height: 300,
+                child: Column(
+                  children: [Text('test'), Text('<${widget.folder.name}>')],
+                ))),
+        SizedBox(
+          height: 10,
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            'start!',
+            style: TextStyle(fontSize: 20),
+          ),
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              minimumSize: Size(200, 50)),
+        ),
+      ],
+    );
+  }
+
+  Widget _NormalUI(List<Word> words, int index) {
+    return Column(
+      children: [
+        Card(
+          child: Text(words[index].english),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        ListView.builder(
+          itemCount: 4,
+          itemBuilder: (context, index) => Card(
+            child: Text(words[index].korean),
+          ),
+        )
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(future: getWord(), builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
+      appBar: AppBar(),
+      body: FutureBuilder(
+        future: getWord(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
-        if (snapshot.hasError) {
-          print(snapshot.error.toString());
-          return Center(
-            child: Text('오류가 발생했습니다.'),
-          );
-        }
-        return Container(
+          if (snapshot.hasError) {
+            print(snapshot.error.toString());
+            return Center(
+              child: Text('오류가 발생했습니다.'),
+            );
+          }
 
-        );
-      },),
+          final words = snapshot.requireData;
+
+          return Center(
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: words.length,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return AnimatedSwitcher(
+                    duration: Duration(milliseconds: 350),
+                    transitionBuilder: (child, animation) {
+                      return SlideTransition(
+                        position: Tween<Offset>(
+                          begin: Offset(1, 0),
+                          end: Offset(1, 0),
+                        ).animate(animation),
+                      );
+                    },
+                    child: _StartUI(),
+                  );
+                } else {
+                  return AnimatedSwitcher(
+                    duration: Duration(milliseconds: 350),
+                    transitionBuilder: (child, animation) {
+                      return SlideTransition(
+                        position: Tween<Offset>(
+                          begin: Offset(1, 0),
+                          end: Offset(0, 0),
+                        ).animate(animation),
+                        child: child,
+                      );
+                    },
+                    child: _NormalUI(words, index),
+                  );
+                }
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
-
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,25 +1,85 @@
+import 'package:flutter/material.dart';
+
 void main() {
-
+  runApp(MyApp());
 }
 
-class Person{
-  String _name;
-  int _age;
-
-  String get name => _name;
-
-  set name(String value) {
-    _name = value;
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: QuizPage(),
+    );
   }
-
-  int get age => _age;
-
-  set age(int value) {
-    _age = value;
-  }
-
-  Person(this._name, this._age);
-
-
 }
 
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  final List<String> _questions = [
+    "문제 1: Flutter란?",
+    "문제 2: Dart란?",
+    "문제 3: StatefulWidget과 StatelessWidget의 차이?",
+  ];
+
+  void _nextQuestion() {
+    if (_currentIndex < _questions.length - 1) {
+      setState(() {
+        _currentIndex++;
+      });
+      _pageController.animateToPage(
+        _currentIndex,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("단어 퀴즈")),
+      body: Center(
+        child: PageView.builder(
+          controller: _pageController,
+          itemCount: _questions.length,
+          itemBuilder: (context, index) {
+            return AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(1, 0),
+                    end: Offset(0, 0),
+                  ).animate(animation),
+                  child: child,
+                );
+              },
+              child: Container(
+                key: ValueKey<int>(index),
+                padding: EdgeInsets.all(16),
+                color: Colors.blueAccent,
+                child: Center(
+                  child: Text(
+                    _questions[index],
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _nextQuestion,
+        child: Icon(Icons.arrow_forward),
+      ),
+    );
+  }
+}
